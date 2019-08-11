cc95d7d1f52538ead5196276bc6818d0638919dd
sgg2123 <sgg2123@columbia.edu>
2019-08-11 13:52:59 -0400

    zippy component

---
+++ src/app/zippy/zippy.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'zippy',
+  templateUrl: './zippy.component.html',
+  styleUrls: ['./zippy.component.css']
+})
+export class ZippyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  @Input('title') title: string;
+  isExpanded: boolean;
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
+}
